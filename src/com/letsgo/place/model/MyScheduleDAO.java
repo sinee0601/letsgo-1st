979ef359191f4eae827824599be8ffa8cb55d2f9
sinee0601 <sinee0601@gmail.com>
2026-04-27 11:37:15 +0900

    스케쥴 삭제 저장 추가

--- src/com/letsgo/place/model/MyScheduleDAO.java
+++ src/com/letsgo/place/model/MyScheduleDAO.java
@@ -8,8 +8,8 @@ import java.util.List;
 
 public class MyScheduleDAO {
 
-	public List<MyScheduleVO> getMyScheduleList(String userId, String keyword, String sortType, String filter) {
-		List<MyScheduleVO> list = new ArrayList<>();
+	public List<MyScheduleVO> getMyScheduleList(String userId, String keyword, String sortType, String sharedFilter) {
+		List<MyScheduleVO> tmp = new ArrayList<>();
 
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT s.MY_SCHEDULE_ID, s.TITLE AS SCH_TITLE, s.START_AT, s.IS_SHARED, ");
@@ -19,7 +19,7 @@ public class MyScheduleDAO {
 		sql.append("JOIN PLACE p ON v.PLACE_ID = p.PLACE_ID ");
 		sql.append("WHERE s.USER_ID = ? ");
 
-		if ("shared".equals(filter)) {
+		if ("shared".equals(sharedFilter)) {
 			sql.append("AND s.IS_SHARED = 1 ");
 		}
 
@@ -27,7 +27,7 @@ public class MyScheduleDAO {
 			sql.append("AND (s.TITLE LIKE ? OR p.TITLE LIKE ?) ");
 		}
 
-		if ("name".equals(sortType)) {
+		if ("title".equals(sortType)) {
 			sql.append("ORDER BY s.TITLE ASC, v.VISIT_ORDER ASC");
 		} else {
 			sql.append("ORDER BY s.START_AT DESC, v.VISIT_ORDER ASC");
@@ -47,14 +47,106 @@ public class MyScheduleDAO {
 			}
 			ResultSet rs = stmt.executeQuery();
 			while (rs.next()) {
-				list.add(new MyScheduleVO(rs.getString("MY_SCHEDULE_ID"), rs.getString("SCH_TITLE"),
+				tmp.add(new MyScheduleVO(rs.getString("MY_SCHEDULE_ID"), rs.getString("SCH_TITLE"),
 						rs.getString("START_AT"), rs.getString("IS_SHARED"), rs.getString("placeId"),
 						rs.getString("PLACE_TITLE"), rs.getString("ADDR1"), rs.getString("FIRST_IMAGE"),
 						rs.getString("PLACE_TYPE")));
 			}
+			stmt.close();
+			conn.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return list;
+		return tmp;
 	}
+
+	public boolean deleteMySchedule(String userId, String scheduleId) {
+		boolean flag = false;
+		try {
+			Connection conn = DBCP.getConnection();
+
+			String sql = "DELETE FROM VISIT_ITEM WHERE MY_SCHEDULE_ID = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, scheduleId);
+			stmt.executeUpdate();
+
+			sql = "DELETE FROM MY_SCHEDULE WHERE MY_SCHEDULE_ID = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, scheduleId);
+			stmt.executeUpdate();
+
+			stmt.close();
+			conn.close();
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+
+	}
+
+	public boolean deleteMyScheduleList(String userId, String[] scheduleId) {
+		boolean flag = false;
+		String sql = "";
+		try {
+			Connection conn = DBCP.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			for (String tmp : scheduleId) {
+				sql = "DELETE FROM VISIT_ITEM WHERE MY_SCHEDULE_ID = ?";
+				stmt = conn.prepareStatement(sql);
+				stmt.setString(1, tmp);
+				stmt.executeUpdate();
+
+				sql = "DELETE FROM MY_SCHEDULE WHERE MY_SCHEDULE_ID = ?";
+				stmt = conn.prepareStatement(sql);
+				stmt.setString(1, tmp);
+				stmt.executeUpdate();
+			}
+			stmt.close();
+			conn.close();
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return flag;
+	}
+
+	public String setMySchedule(String[] visitItemId, String[] visitOrder, String[] distanceToNext, String scheduleId,
+			String title, String startAt, String budgetDetail, String todoDetail, String userId, int isShared) {
+		String str = null;
+
+		String sql = "";
+
+		try {
+			Connection conn = DBCP.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			for (int i = 0; i < visitItemId.length; i++) {
+
+				sql = "UPDATE VISIT_ITEM SET VISIT_ORDER = ?, DISTANCE_TO_NEXT = ? WHERE VISIT_ITEM_ID = ?";
+				stmt = conn.prepareStatement(sql);
+				stmt.setString(1, visitItemId[i]);
+				stmt.setString(2, visitOrder[i]);
+				stmt.setString(3, distanceToNext[i]);
+				stmt.executeUpdate();
+			}
+			sql = "UPDATE MY_SCHEDULE SET TITLE = ?, START_AT = ?, BUDGET_DETAILS = ?, TODO_DETAILS = ?, IS_SHARED = ? WHERE MY_SCHEDULE_ID = ?' AND USER_ID = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setString(1, title);
+			stmt.setString(2, startAt);
+			stmt.setString(3, budgetDetail);
+			stmt.setString(4, todoDetail);
+			stmt.setLong(5, isShared);
+			stmt.setString(6, scheduleId);
+			stmt.setString(7, userId);
+
+			stmt.close();
+			conn.close();
+			str = userId;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return str;
+	}
+
 }
