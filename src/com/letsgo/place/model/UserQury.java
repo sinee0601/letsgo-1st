cf099ae61e3d957512ec8c90e701eaa076ac9f59
cloud44112 <cloud44112@gmail.com>
2026-04-28 16:05:44 +0900

    userSQL 수정

--- src/com/letsgo/place/model/UserQury.java
+++ src/com/letsgo/place/model/UserQury.java
@@ -1,6 +1,6 @@
 package com.letsgo.place.model;
 
-public class UserQury {
+public interface UserQury {
 	String LOGIN_SQL = "SELECT NAME, EMAIL FROM USERS WHERE USER_ID = ? AND PASSWORD = ?";
 	String SIGNUP_SQL = "INSERT INTO USERS (USER_ID, EMAIL, NAME, PASSWORD) VALUES (?,?,?,?)";
 	String IDCHECK_SQL = "SELECT 1 FROM USERS WHERE USER_ID = ?";
