708ebab1dd00cd9c8730cd6659e945dd5c534653
Mskko <mskk0410@gmail.com>
2026-04-29 10:12:58 +0900

    RouteScheduleVO : Junit테스트용으로 toString 만들었다가 원복해놓음 PostScheduleService : 다 추가해놓음 TestPostScheduleService : 테스트코드

--- src/com/letsgo/place/service/PostScheduleService.java
+++ src/com/letsgo/place/service/PostScheduleService.java
@@ -2,9 +2,14 @@ package com.letsgo.place.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.letsgo.place.model.DBCP;
+import com.letsgo.place.model.MapScheduleVO;
 import com.letsgo.place.model.PostScheduleDAO;
+import com.letsgo.place.model.PostScheduleVO;
+import com.letsgo.place.model.RouteScheduleVO;
 
 
 public class PostScheduleService {
@@ -13,7 +18,7 @@ public class PostScheduleService {
 	private Connection conn;
 
 
-	public PostScheduleService() throws Exception {
+	public PostScheduleService() throws ClassNotFoundException, SQLException {
 		conn = DBCP.getConnection();
 		dao = new PostScheduleDAO(conn);
 	}
@@ -38,6 +43,37 @@ public class PostScheduleService {
 		}
 		return result;
 	}
+	
+	
+	public List<PostScheduleVO> getPostScheduleList(String keyword, String searchType, String sortType) {
+		return dao.getPostScheduleList(keyword, searchType, sortType);
+	}
+	
+	public List<PostScheduleVO> getUserPostScheduleList(String userId, String keyword, String searchType, String sortType) {
+		return dao.getUserPostScheduleList(userId, keyword, searchType, sortType);
+	}
+	
+	public String getBudgetDetail(String postId) {
+		return dao.getBudgetDetail(postId);
+	}
+	
+	public String getTodoDetail(String postId) {
+		return dao.getTodoDetail(postId);
+	}
+	
+	public List<RouteScheduleVO> getScheduleRoute(String postId) {
+		return dao.getScheduleRoute(postId);
+	}
+	
+	public List<MapScheduleVO> getMapSchedule(String postId) {
+		return dao.getMapSchedule(postId);
+	}
+	
+	public boolean plusLike(String postId) {
+		return dao.plusLike(postId);
+	}
+	
+	
 	
 	
 }
--- src/test/com/letsgo/place/model/TestPostScheduleService.java
+++ src/test/com/letsgo/place/model/TestPostScheduleService.java
@@ -2,13 +2,71 @@ package test.com.letsgo.place.model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.letsgo.place.model.MapScheduleVO;
+import com.letsgo.place.model.RouteScheduleVO;
+import com.letsgo.place.service.PostScheduleService;
+
 
 public class TestPostScheduleService {
 
+	private PostScheduleService service;
+	
+	
+	@Before
+	public void setup() throws Exception{
+		service = new PostScheduleService();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void deletePostSchedule() {
+		assertTrue(service.deletePostSchedule("P002"));
+	}
+	
+	@Test
+	public void getPostScheduleList() {
+		assertNotNull(service.getPostScheduleList("", "", "latest"));
+	}
+	
+	@Test
+	public void getUserPostScheduleList() {
+		assertNotNull(service.getUserPostScheduleList("user01", "", "", "latest"));
+	}
+	
+	@Test
+	public void getBudgetDetail() {
+		assertEquals("삼겹살 마구 먹기", service.getBudgetDetail("P023"));
+		//assertNotEquals("삼겹살 마구 먹기", service.getBudgetDetail("P023"));
+	}
+	
+	@Test
+	public void getTodoDetail() {
+		assertEquals("햄부기 사냥함부기", service.getTodoDetail("P023"));
+		//assertNotEquals("햄부기 사냥햄부기", service.getTodoDetail("P023"));
+	}
+	
+	@Test
+	public void getScheduleRoute() {
+		//assertEquals(new ArrayList<RouteScheduleVO>(), service.getScheduleRoute("P023"));
+		List<RouteScheduleVO> list = service.getScheduleRoute("P023");
+		System.out.println(list);
+			
+	}
+	
+	@Test
+	public void getMapSchedule() {
+		List<MapScheduleVO> mapList = service.getMapSchedule("P023");
+		assertNotNull(mapList);
+	}
+	
+	@Test
+	public void plusLike() {
+		assertTrue(service.plusLike("P023"));
 	}
 
 }
