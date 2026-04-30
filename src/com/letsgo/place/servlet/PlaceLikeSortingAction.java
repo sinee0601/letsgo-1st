c88bccc2d9233e21cd3885f71425599ef78ed605
Seeun <happysarahkim2004@gmail.com>
2026-04-30 12:44:29 +0900

    플레이스 좋아요 추가

--- src/com/letsgo/place/servlet/PlaceLikeSortingAction.java
+++ src/com/letsgo/place/servlet/PlaceLikeSortingAction.java
@@ -14,11 +14,42 @@ public class PlaceLikeSortingAction implements Action {
             throws ServletException, IOException, ClassNotFoundException, SQLException {
         
         String sort = request.getParameter("sortOrder");
+        String redirectCmd = request.getParameter("redirectCmd");
+        
         if (sort == null || sort.trim().isEmpty()) {
             sort = "distance";
         }
         
         PlaceService placeService = new PlaceService();
+        
+        if ("restaurantUI".equals(redirectCmd)) {
+            if ("like".equals(sort)) {
+                request.setAttribute("restaurantPlaceList", placeService.getPlaceOrderByLike("RESTAURANT"));
+            } else {
+                request.setAttribute("restaurantPlaceList", placeService.getPlaceOrderByTitle("RESTAURANT"));
+            }
+            request.setAttribute("sortOrder", sort);
+            return "restaurant.jsp";
+            
+        } else if ("stayUI".equals(redirectCmd)) {
+            if ("like".equals(sort)) {
+                request.setAttribute("stayPlaceList", placeService.getPlaceOrderByLike("STAY"));
+            } else {
+                request.setAttribute("stayPlaceList", placeService.getPlaceOrderByTitle("STAY"));
+            }
+            request.setAttribute("sortOrder", sort);
+            return "stay.jsp";
+            
+        } else if ("leisureUI".equals(redirectCmd)) {
+            if ("like".equals(sort)) {
+                request.setAttribute("leisurePlaceList", placeService.getPlaceOrderByLike("LEISURE"));
+            } else {
+                request.setAttribute("leisurePlaceList", placeService.getLeisurePlaces());
+            }
+            request.setAttribute("sortOrder", sort);
+            return "leisure.jsp";
+        }
+        
         if ("like".equals(sort)) {
             request.setAttribute("leisurePlaceList", placeService.getLeisurePlacesOrderByLikeDesc());
         } else {
