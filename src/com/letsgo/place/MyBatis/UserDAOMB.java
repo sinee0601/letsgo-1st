2cea98d8f5c74afa515c3c0e2b78bd317d4b321a
Seeun <happysarahkim2004@gmail.com>
2026-05-07 18:16:58 +0900

    유저 MyBatis 완

--- src/com/letsgo/place/MyBatis/UserDAOMB.java
+++ src/com/letsgo/place/MyBatis/UserDAOMB.java
@@ -1,5 +1,8 @@
 package com.letsgo.place.MyBatis;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.letsgo.place.DBCPMybatis.DBCPMybatis;
 import com.letsgo.place.model.UserDAOInterface;
 import com.letsgo.place.model.UserVO;
 
@@ -7,32 +10,44 @@ public class UserDAOMB implements UserDAOInterface {
 
 	@Override
 	public UserVO login(String userID, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		SqlSession session = DBCPMybatis.getSqlSessionFactory().openSession();
+		UserVO result = session.selectOne("userMapper.login", new UserVO(userID, null, null, password));
+		session.close();
+		return result;
 	}
 
 	@Override
 	public boolean signup(String userID, String email, String name, String password) {
-		// TODO Auto-generated method stub
-		return false;
+		SqlSession session = DBCPMybatis.getSqlSessionFactory().openSession();
+		boolean result = session.insert("userMapper.signUp", new UserVO(userID, email, name, password)) == 1;
+		if(result)session.commit();
+		session.close();
+		return result;
 	}
 
 	@Override
 	public boolean idcheck(String userID) {
-		// TODO Auto-generated method stub
-		return false;
+		SqlSession session = DBCPMybatis.getSqlSessionFactory().openSession();
+		boolean result = (session.selectOne("userMapper.idCheck", userID) != null);
+		session.close();
+		return result;
 	}
 
 	@Override
 	public boolean updatePassword(String userID, String email, String newPassword) {
-		// TODO Auto-generated method stub
-		return false;
+		SqlSession session = DBCPMybatis.getSqlSessionFactory().openSession();
+		boolean result = session.update("userMapper.updatePassword", new UserVO(userID, email, null, newPassword)) == 1;
+		if(result)session.commit();
+		session.close();
+		return result;
 	}
 
 	@Override
 	public String findUserIdByNameAndEmail(String name, String email) {
-		// TODO Auto-generated method stub
-		return null;
+		SqlSession session = DBCPMybatis.getSqlSessionFactory().openSession();
+		String result = session.selectOne("userMapper.findUserId", new UserVO(null, email, name, null));
+		session.close();
+		return result;
 	}
 
 }
