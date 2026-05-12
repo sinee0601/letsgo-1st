fb83929c3aa48a8e5158d780324cdb6956634037
sinee0601 <sinee0601@gmail.com>
2026-05-12 09:05:31 +0900

    Update LeisureUIAction.java

--- src/com/letsgo/place/servlet/LeisureUIAction.java
+++ src/com/letsgo/place/servlet/LeisureUIAction.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import com.letsgo.place.mybatis.service.MyScheduleServiceMB;
 import com.letsgo.place.mybatis.service.PlaceServiceMB;
 import com.letsgo.place.model.vo.PlaceVO;
 import com.letsgo.place.model.vo.RouteScheduleVO;
